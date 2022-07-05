5dfb51e5580f716f8d0362ad6c6d51dfabf56e9d
Tomilov Algernon Andrey <thekeibee@yandex.ru>
2022-07-05 23:18:23 +0800

    ClickListener on Like and share

--- app/src/main/java/com/example/nmedia/CountLikeShare.kt
+++ app/src/main/java/com/example/nmedia/CountLikeShare.kt
@@ -6,10 +6,10 @@ import java.lang.Math.round
 fun formatCount(count: Long): String {
     val df = DecimalFormat("#.#")
     return when (count) {
-        in 0..999 -> count.toString() // до 999
+        in 0..999 -> count.toString()
         in 1000..999_99 -> "${
-            round(count.toDouble() / 1000).toLong()}K" // до 99К
-        in 100_000..999_999 -> "${(count.toDouble() / 1000).toLong()}K" // до 999К
+            round(count.toDouble() / 1000).toLong()}K"
+        in 100_000..999_999 -> "${(count.toDouble() / 1000).toLong()}K"
         in 1_000_000..9_000_000 -> "${
             df.format(count.toDouble() / 1000000).toString().toLong()
         }M"
